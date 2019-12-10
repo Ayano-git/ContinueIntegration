40753011664413742c3dc0b07b81cd702d07f4eb
Anthony <anthonyverret2436@gmail.com>
2019-12-10 11:48:52 +0100

    Deploy master branches to URL named after branch

--- scripts/deploy_review_app.sh
+++ scripts/deploy_review_app.sh
@@ -1,3 +1,7 @@
 echo "{ \"alias\": \"Ayano-$TRAVIS_BRANCH\" }" > now-review-app.json
 now --public --token $NOW_TOKEN
 now alias --token $NOW_TOKEN --local-config now-review-app.json
+
+echo "{ "alias": "staging-Ayano" }" > now-staging-app.json
+now --public --token $NOW_TOKEN
+now alias --token $NOW_TOKEN --local-config now-staging-app.json
